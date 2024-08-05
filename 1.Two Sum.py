82a8c39120a3c5df05e70eb0fda9e5f550ba0822
Haripriya420 <159681636+Haripriya420@users.noreply.github.com>
2024-08-05 08:39:57 -0700

    1.Two Sum.py

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -32,6 +32,6 @@ class Solution(object):
         """
         for i in range(len(nums)):
             for j in range(1,len(nums)):
-                if(nums[i]+nums[j]==target):
+                if((nums[i]+nums[j]==target) and i!=j):
                     return [i,j]
         
